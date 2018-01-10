20e07e21b9944d75faf5d014d4c9b58e25322cae
Ryan Coopersmith <ryanc@moven.com>
2018-01-10 13:39:45 -0500

    Add file to print JSON data in JSON string format

--- SimpleJSON/Main.hs
+++ SimpleJSON/Main.hs
@@ -2,5 +2,4 @@ module Main (main) where
 
 import SimpleJSON
 
--- jsonTest = (JObject (("myArrayValue", JArray ((JNumber 1):(JNumber 2):(JNumber 3):[])):("myNullValue", JNull):("myBoolValue", JBool True):[]))
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = print (JObject (("myArrayValue", JArray ((JNumber 1):(JNumber 2):(JNumber 3):[])):("myNullValue", JNull):("myBoolValue", JBool True):[]))
