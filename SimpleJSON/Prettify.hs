d91ced41a769afcd62cee6815f55b647b1f068da
Ryan Coopersmith <ryanc@moven.com>
2018-01-10 21:57:41 -0500

    More pretty printing funcs

--- SimpleJSON/Prettify.hs
+++ SimpleJSON/Prettify.hs
@@ -3,8 +3,11 @@ module Prettify (
     (<>),
     char,
     double,
+    fsep,
+    hcat,
     punctuate
     text,
+    compact
 ) where
 
 data Doc = Empty
@@ -45,6 +48,15 @@ x </> y = x <> softline <> y
 softline :: Doc
 softline = group line
 
+group :: Doc -> Doc
+group x = flatten x `Union` x
+
+flatten :: Doc -> Doc
+flatten (x `Concat` y) = flatten x `Concat` flatten y
+flatten Line           = Char ' '
+flatten (x `Union` _)  = flatten x
+flatten other          = other
+
 hcat :: [Doc] -> Doc
 hcat = fold (<>)
 
@@ -55,3 +67,32 @@ punctuate :: Doc -> [Doc] -> [Doc]
 punctuate _ []     = []
 punctuate _ [d]    = [d]
 punctuate p ds = map (<> p) ds
+
+compact :: Doc -> String
+compact x = transform [x]
+    where transform [] = ""
+          transform (d:ds) =
+              case d of
+                Empty        -> transform ds
+                Char c       -> c : transform ds
+                Text s       -> s ++ transform ds
+                Line         -> '\n' : transform ds
+                a `Concat` b -> transform (a:b:ds)
+                _ `Union` b  -> transform (b:ds)
+
+pretty :: Int -> Doc -> String
+pretty width x = best 0 [x]
+    where best col (d:ds) =
+              case d of
+                Empty        -> best col ds
+                Char c       -> c :  best (col + 1) ds
+                Text s       -> s ++ best (col + length s) ds
+                Line         -> '\n' : best 0 ds
+                a `Concat` b -> best col (a:b:ds)
+                a `Union` b  -> nicest col (best col (a:ds))
+                                           (best col (b:ds))
+          best _ _ = ""
+
+          nicest col a b | (width - least) `fits` a = a
+                         | otherwise                = b
+                         where least = min width col
