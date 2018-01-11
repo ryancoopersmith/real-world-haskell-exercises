258e78ce2ebdfee5241baec84e06c21ca1db4f42
Ryan Coopersmith <ryanc@moven.com>
2018-01-11 16:19:30 -0500

    Set pretty print width via arg

--- SimpleJSON/Main.hs
+++ SimpleJSON/Main.hs
@@ -1,5 +1,7 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+
 import SimpleJSON
 -- import PutJSON
 import PrettyJSON (ppJValue, compactJValue)
@@ -7,5 +9,14 @@ import PrettyJSON (ppJValue, compactJValue)
 json = (JObject (("myArrayValue", JArray ((JNumber 1):(JNumber 2):(JNumber 3):[])):("myNullValue", JNull):("myBoolValue", JBool True):[]))
 
 -- main = putJValue json
-main = ppJValue 80 json
--- main = compactJValue json
+
+main = do
+    putStrLn "compact:"
+    compactJValue json
+
+    putStrLn "pretty:"
+    width <- getIntArg
+    ppJValue width json
+
+getIntArg :: IO Int
+getIntArg = fmap (read . head) getArgs
