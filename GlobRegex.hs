50841fac21640177d91587de8b5e161c5f9b8301
Ryan Coopersmith <ryanc@moven.com>
2018-01-13 21:27:13 -0500

    More work on GlobRegex module

--- GlobRegex.hs
+++ GlobRegex.hs
@@ -3,7 +3,7 @@ module GlobRegex (
     matchesGlob
 ) where
 
-import Text.Regex.Posix ((=~))
+import Text.Regex.Posix
 
 globToRegex :: String -> String
 globToRegex cs = '^' : globToRegex' cs ++ "$"
@@ -20,3 +20,26 @@ globToRegex' ('[':c:cs)     = '['  :  c : charClass cs
 globToRegex' ('[':_)        = error "unterminated character class"
 
 globToRegex' (c:cs) = escape c ++ globToRegex' cs
+
+escape :: Char -> String
+escape c | c `elem` regexChars = '\\' : [c]
+         | otherwise = [c]
+    where regexChars = "\\+()^$.{}]|"
+
+charClass :: String -> String
+charClass (']':cs) = ']' : globToRegex' cs
+charClass (c:cs)   = c : charClass cs
+charClass []       = error "unterminated character class"
+
+matchesGlob :: FilePath -> String -> Bool -> Bool
+matchesGlob name pat ins = if ins
+                           then name =~+ (globToRegex pat, compIgnoreCase, execBlank)
+                           else name =~ globToRegex pat
+
+-- my case insensitive regex matcher function
+(=~+) :: (RegexMaker regex compOpt execOpt source,
+          RegexContext regex source1 target)
+         => source1 -> (source, compOpt, execOpt) -> target
+
+source1 =~+ (source, compOpt, execOpt)
+        = match (makeRegexOpts compOpt execOpt source) source1
