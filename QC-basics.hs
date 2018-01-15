c62c01252c02147e92c77b3d594a833d78685186
Ryan Coopersmith <ryanc@moven.com>
2018-01-15 14:29:33 -0500

    ADD QC-basics file for QuickCheck examples

---
+++ QC-basics.hs
@@ -0,0 +1,34 @@
+import Test.QuickCheck
+import Data.List
+
+-- never seen quicksort written in so few lines!
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
+
+-- idempotency invariant property
+prop_idempotent xs = qsort (qsort xs) == qsort xs
+
+-- w/o implication (throws err for empty list)
+prop_minimum xs = head (qsort xs) == minimum xs
+-- w/ implication of non-empty list
+prop_minimum' xs = not (null xs) ==> head (qsort xs) == minimum xs
+
+prop_ordered xs = ordered (qsort xs)
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
+
+prop_permutation xs = permutation xs (qsort xs)
+    where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+
+prop_maximum xs         =
+    not (null xs) ==>
+        last (qsort xs) == maximum xs
+
+prop_append xs ys       =
+    not (null xs) ==>
+    not (null ys) ==>
+        head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
