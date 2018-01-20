447bcf0484dcd73fc2c2505dde95de7d4b4e6804
Ryan Coopersmith <ryanc@moven.com>
2018-01-20 18:51:58 -0500

    Finish carrier program

--- Carrier.hs
+++ Carrier.hs
@@ -14,6 +14,8 @@ findCarrierBillingAddress :: PersonName
                           -> M.Map MobileCarrier BillingAddress
                           -> Maybe BillingAddress
 
+findCarrierBillingAddress = variation3
+
 variation1 person phoneMap carrierMap addressMap =
     case M.lookup person phoneMap of
       Nothing -> Nothing
@@ -27,3 +29,15 @@ variation2 person phoneMap carrierMap addressMap = do
   carrier <- M.lookup number carrierMap
   address <- M.lookup carrier addressMap
   return address
+
+-- return function isn't necessary here, we should omit it as show below
+
+variation2a person phoneMap carrierMap addressMap = do
+  number <- M.lookup person phoneMap
+  carrier <- M.lookup number carrierMap
+  M.lookup carrier addressMap
+
+-- flip the M.lookup parameters so that we can partially apply the function
+variation3 person phoneMap carrierMap addressMap =
+    lookup phoneMap person >>= lookup carrierMap >>= lookup addressMap
+  where lookup m k = flip M.lookup m k
